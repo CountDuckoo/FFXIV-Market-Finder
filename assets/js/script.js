4fd0269f5ba2933067256b5d9466ed7c182ad930
CountDuckoo <countsuperc@gmail.com>
2023-12-07 18:52:15 -0600

    Added starter code and comments

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// button listener to get the inputs
+// determine what fetch we need to make from those inputs
+// transfer that data to make a request to the other API
+// display the results on the page
